b3fbd94bc708a82a390cd1c0b9dd15d440950176
joshua220508 <jesaenz@uamv.edu.ni>
2026-08-21 18:28:51 -0600

    descargar scenebuilder

---
+++ src/main/java/com/example/registro_app/EstudianteApplication.java
@@ -0,0 +1,19 @@
+package com.example.registro_app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EstudianteApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(EstudianteApplication.class.getResource("estudiante-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Universidad Americana");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
